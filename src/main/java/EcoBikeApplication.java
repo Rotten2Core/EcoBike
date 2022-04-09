c39e84b564922a1893a27f99c0c035bd4cb02395
Serhii Kozyr <kozyr.sergej@gmail.com>
2022-04-09 22:14:14 +0300

    Test

--- src/main/java/EcoBikeApplication.java
+++ src/main/java/EcoBikeApplication.java
@@ -6,11 +6,11 @@ public class EcoBikeApplication {
 
     public static void main(String[] args) {
 
-        try {
-            FileReader fileReader = new FileReader("/home/sergey/workspace/ecobike/target/classes/ecobike.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FileReader fileReader = new FileReader("/home/sergey/workspace/ecobike/target/classes/ecobike.txt");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
 
     }
